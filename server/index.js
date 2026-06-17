e0180b8892b179cf541f909595753921b4e2960b
Porra Mundial <porra@mundial2026.com>
2026-06-17 09:15:54 +0200

    Increase JSON body limit to 5mb for restore endpoint

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(cors({
   },
   credentials: true,
 }));
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 app.use(session({
   secret: 'porra-mundial-2026-secret',
   resave: false,
